74b3b10c21c98c3564640305319a0b7c313a9322
yurigmaia <yuri.g.maia96@gmail.com>
2025-08-15 10:20:58 -0300

    initial commit: configuração inicial do projeto

---
+++ src/test/java/br/com/nowstock/now_stock/NowStockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nowstock.now_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
